7135548f5b1a87cd9d9d973dcf2c01bb59db2f77
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-07-04 17:45:13 +0800

    Update forgotpassword.php

--- forgotpassword.php
+++ forgotpassword.php
@@ -28,36 +28,39 @@
 include('admin/dbcon.php');
 
 
-$username = $_POST['username'];
-
 if(!empty($_POST['username']) && isset($_POST['username']))
     {
-    	
-    	//checks if username in mysql
- 		$sql = "SELECT customer_id,customer_email, customer_password FROM customer WHERE customer_email = '$email' AND customer_password = '$psword'";
- 		$data = $connection->query($sql);
- 		 //var_dump($data);
 
- 	   //if matched login	 
- 	   if($data->num_rows > 0)
-       {
-       		session_start();
-       		$row = $data->fetch_assoc();
-       		$_SESSION["customer_id"] = $row['customer_id'];
-    
-       		echo '<script type="text/javascript">
-				alert("Welcome!");
-				window.location.href = "home.php";
-				</script>';
-       }
-       //else not registered
-       else
-       {
-       		echo '<script type="text/javascript">
-				alert("Please Register Your Account!");
-				window.location.href = "login.php";
-				</script>';
-       }
 	
+    	$username = $_POST['username'];
+
+
+		 /* student */
+		 $query = "SELECT * FROM student WHERE username='$username'";
+		 $result = mysqli_query($conn,$query)or die(mysqli_error());
+		 $row = mysqli_fetch_array($result);
+		 $num_row = mysqli_num_rows($result);
+
+		 /* teacher */
+		 $query_teacher = mysqli_query($conn,"SELECT * FROM teacher WHERE username='$username'")or die(mysqli_error());
+		 $num_row_teacher = mysqli_num_rows($query_teacher);
+		 $row_teacher = mysqli_fetch_array($query_teacher);
+
+		if( $num_row > 0 ) { 
+		
+			//for debugging
+			echo 'true_student';
+			echo $username;
+		}else if ($num_row_teacher > 0){
+
+			//for debugging
+			echo 'true';
+			echo $username;
+		}
+		else
+		{ 
+			
+			echo 'false';
+		}	
 	}
 ?>
