4844f7a38be0e5f4c406647ed8a94308d3b51bc0
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-07-04 13:34:43 +0800

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -3,6 +3,7 @@ include('dbcon.php');
 include('session.php');
 mysqli_query($conn,"update user_log set logout_Date = NOW() where user_id = '$session_id' ")or die(mysqli_error());
 
- session_destroy();
+session_destroy();
 header('location:index.php'); 
+exit();
 ?>
