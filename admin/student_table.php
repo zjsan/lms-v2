60f146f939bffbd6307322e9cdcd9fdf08f34d61
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-07-04 10:51:58 +0800

    Update student_table.php

--- admin/student_table.php
+++ admin/student_table.php
@@ -2,29 +2,18 @@
 	<form action="delete_student.php" method="post">
 	<table cellpadding="0" cellspacing="0" border="0" class="table" id="example">
 	<a data-toggle="modal" href="#student_delete" id="delete"  class="btn btn-danger" name=""><i class="icon-trash icon-large"></i></a>
-	<div class="pull-right">
-			    <ul class="nav nav-pills">
-				<li class="active">
-					<a href="students.php">All</a>
-				</li>
-				<li class="">
-					<a href="unreg_students.php">Unregistered</a>
-				</li>
-				<li class="">
-				<a href="reg_students.php">Registered</a>
-				</li>
-				</ul>
-	</div>
+
 	<?php include('modal_delete.php'); ?>
 		<thead>
 		<tr>
 					<th></th>
 				
-					<th>Name</th>
 					<th>ID Number</th>
+					<th>Name</th>
+					<th>Password</th>
+					<th>Username</th>
 			
 					<th>Course Yr & Section</th>
-					<th></th>
 		</tr>
 		</thead>
 		<tbody>
@@ -39,8 +28,14 @@
 		<td width="30">
 		<input id="optionsCheckbox" class="uniform_on" name="selector[]" type="checkbox" value="<?php echo $id; ?>">
 		</td>
-	
+		
+		
+		<td><?php echo $row['student_id'] //inserted student id?></td> 
+
 		<td><?php echo $row['firstname'] . " " . $row['lastname']; ?></td> 
+
+		<td><?php echo $row['password'] //password?></td> 
+
 		<td><?php echo $row['username']; ?></td> 
 	
 		<td width="100"><?php echo $row['class_name']; ?></td> 
