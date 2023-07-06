221d2878ee8c8c40326d9fe80485013942388b34
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-07-06 11:17:38 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,35 +1,29 @@
 <?php
+    include('admin/dbcon.php');
+    session_start();
 
-			
-		include('session.php');
-		include('admin/dbcon.php');
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-		$username = $_POST['username'];
-		$password = $_POST['password'];
+    // Check for student
+    $studentQuery = "SELECT * FROM student WHERE username='$username' AND password='$password'";
+    $studentResult = mysqli_query($conn, $studentQuery) or die(mysqli_error($conn));
+    $studentRow = mysqli_fetch_array($studentResult);
+    $numStudentRows = mysqli_num_rows($studentResult);
 
-		if(!empty($password)) {
-			/* student */
-			$query = "SELECT * FROM student WHERE username='$username' AND password='$password' AND `status`='Registered'";
-			$result = mysqli_query($conn,$query)or die(mysqli_error());
-			$row = mysqli_fetch_array($result);
-			$num_row = mysqli_num_rows($result);
+    // Check for teacher
+    $teacherQuery = "SELECT * FROM teacher WHERE username='$username' AND password='$password'";
+    $teacherResult = mysqli_query($conn, $teacherQuery) or die(mysqli_error($conn));
+    $teacherRow = mysqli_fetch_array($teacherResult);
+    $numTeacherRows = mysqli_num_rows($teacherResult);
 
-			/* teacher */
-			$query_teacher = mysqli_query($conn,"SELECT * FROM teacher WHERE username='$username' AND password='$password' AND `teacher_status`='Registered' AND teacher_stat NOT IN('Deactivated')")or die(mysqli_error());
-			$num_row_teacher = mysqli_num_rows($query_teacher);
-			$row_teahcer = mysqli_fetch_array($query_teacher);
-
-			if( $num_row > 0 ) { 
-				$_SESSION['id']=$row['student_id'];
-				echo 'true_student';	
-			}else if ($num_row_teacher > 0){
-				$_SESSION['id']=$row_teahcer['teacher_id'];
-				echo 'true';
-			
-			}else{ 
-					echo 'false';
-			}	
-		} else {
-			echo 'false';
-		}
-		?>
+    if ($numStudentRows > 0) {
+        $_SESSION['id'] = $studentRow['student_id'];
+        echo 'true_student';
+    } else if ($numTeacherRows > 0) {
+        $_SESSION['id'] = $teacherRow['teacher_id'];
+        echo 'true';
+    } else {
+        echo 'false';
+    }
+?>
