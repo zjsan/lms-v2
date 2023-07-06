ea765e410ee6d49c96c2673d68479e2516eae644
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-07-06 17:11:03 +0800

    added insert logs and logout

--- login.php
+++ login.php
@@ -8,25 +8,28 @@
     // Check for student
     $studentQuery = "SELECT * FROM student WHERE username='$username' AND password='$password'";
     $studentResult = mysqli_query($conn, $studentQuery) or die(mysqli_error($conn));
-    $studentRow = mysqli_fetch_array($studentResult);
     $numStudentRows = mysqli_num_rows($studentResult);
 
     // Check for teacher
     $teacherQuery = "SELECT * FROM teacher WHERE username='$username' AND password='$password'";
     $teacherResult = mysqli_query($conn, $teacherQuery) or die(mysqli_error($conn));
-    $teacherRow = mysqli_fetch_array($teacherResult);
     $numTeacherRows = mysqli_num_rows($teacherResult);
 
     if ($numStudentRows > 0) {
+        $studentRow = mysqli_fetch_array($studentResult);
         $_SESSION['id'] = $studentRow['student_id'];
 
-        mysqli_query($conn,"SELECT student.username, user_log.login_date, user_log.logout_date
-									FROM student INNER JOIN user_log ON student.student_id = user_log.user_log_id") or die(mysqli_error());
-	    mysqli_query($conn,"insert into user_log (username,login_date,user_id) values('$username',NOW(),".$studentRow['student_id'].")")or die(mysqli_error());
+        $insertQuery = "INSERT INTO user_log (username, login_date, user_id) VALUES ('$username', NOW(), ".$studentRow['student_id'].")";
+        mysqli_query($conn, $insertQuery) or die(mysqli_error($conn));
         echo 'true';
-            
+
     } else if ($numTeacherRows > 0) {
+        $teacherRow = mysqli_fetch_array($teacherResult);
         $_SESSION['id'] = $teacherRow['teacher_id'];
+
+        $insertQuery = "INSERT INTO user_log (username, login_date, user_id) VALUES ('$username', NOW(), ".$teacherRow['teacher_id'].")";
+        mysqli_query($conn, $insertQuery) or die(mysqli_error($conn));
+
         echo 'true';
     } else {
         echo 'false';
--- logout.php
+++ logout.php
@@ -1,5 +1,6 @@
 <?php
 session_destroy();
 header('location:index.php');
+mysqli_query($conn,"INSERT INTO user_log(logout_Date) values(NOW()) where user_id = '$session_id'")or die(mysqli_error());
 exit();
 ?>
