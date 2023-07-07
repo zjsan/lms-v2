a4ca8629beec5164404fd2787201bbc649c29980
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-07-07 13:17:32 +0800

    Update dasboard_teacher.php

--- dasboard_teacher.php
+++ dasboard_teacher.php
@@ -4,7 +4,7 @@
 		<?php include('navbar_teacher.php'); ?>
         <div class="container-fluid">
             <div class="row-fluid">
-				<?php include('teacher_sidebar.php'); ?>
+			<?php include('teacher_sidebar.php'); ?>
                 <div class="span6" id="content">
                      <div class="row-fluid">
 					    <!-- breadcrumb -->	
