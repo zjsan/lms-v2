36ba5f350181aebfef2f41a31998b89021cba02e
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-07-07 12:45:41 +0800

    Update teacher_class.php

--- teacher_class.php
+++ teacher_class.php
@@ -1,10 +1,14 @@
 			
 						
 						<ul	 id="da-thumbs" class="da-thumbs">
-										<?php $query = mysqli_query($conn,"select * from teacher_class
+										<?php
+										
+										$select = "SELECT * FROM teacher_class
 										LEFT JOIN class ON class.class_id = teacher_class.class_id
 										LEFT JOIN subject ON subject.subject_id = teacher_class.subject_id
-										where teacher_id = '$session_id' and school_year = '$school_year' ")or die(mysqli_error());
+										where teacher_id = '$session_id' and school_year = '$school_year' ";
+
+										$query = mysqli_query($conn,$select)or die(mysqli_error());
 										$count = mysqli_num_rows($query);
 										
 										if ($count > 0){
